a973d5f5c7d240b501adff689cce069a361c57ab
Assiatou01 <assisalahbah@gmail.com>
2026-09-19 17:12:23 +0000

    feat(evaluation-service): add OpenFeign, Zipkin tracing and AI client

--- backend/evaluation-service/src/main/java/com/esmt/labstn/evaluation/EvaluationServiceApplication.java
+++ backend/evaluation-service/src/main/java/com/esmt/labstn/evaluation/EvaluationServiceApplication.java
@@ -2,12 +2,15 @@ package com.esmt.labstn.evaluation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class EvaluationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EvaluationServiceApplication.class, args);
     }
-
 }
