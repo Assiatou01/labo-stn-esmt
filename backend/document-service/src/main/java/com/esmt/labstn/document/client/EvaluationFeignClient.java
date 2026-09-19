57bdb458d5edc1d6561b4c93965dbb5b6bc59391
Assiatou01 <assisalahbah@gmail.com>
2026-09-19 17:10:30 +0000

    feat(document-service): add OpenFeign, Zipkin tracing and Evaluation client

---
+++ backend/document-service/src/main/java/com/esmt/labstn/document/client/EvaluationFeignClient.java
@@ -0,0 +1,13 @@
+package com.esmt.labstn.document.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Map;
+
+@FeignClient(name = "EVALUATION-SERVICE")
+public interface EvaluationFeignClient {
+    @GetMapping("/api/v1/evaluation/{id}")
+    Map<String, Object> getEvaluationDetails(@PathVariable("id") Long id);
+}
